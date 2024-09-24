f3c4e74bff8b675cb6850b8874c8eb430f64825e
qing <judy88622@gmail.com>
2024-09-25 05:21:52 +0800

    Modify header

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,13 +5,39 @@
     <a href="/" class="text-white text-lg font-bold hover:underline">
       qing's blog
     </a>
-    <nav class="text-white space-x-4">
-      <a href="#about" class="hover:underline">關於</a>
-      <a href="#articles" class="hover:underline">文章列表</a>
-      <a href="#categories" class="hover:underline">分類</a>
+    <nav class="text-white space-x-4 aa">
+      <div v-for="item in nav" :key="item.href" :href="item.href" class="px-3">
+        <a class="nav">{{ item.text }}</a>
+      </div>
     </nav>
   </header>
 
   <slot />
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const nav = [
+  {
+    text:"關於",
+    href:"#about"
+  },
+  {
+    text:"文章列表",
+    href:"#about"
+  },
+  {
+    text:"分類",
+    href:"#about"
+  },
+]
+</script>
+<style>
+.nav:hover{
+  color: rgb(237, 226, 132);
+  font-weight: 600;
+  /* font-size: 1.1em; */
+}
+.aa{
+  display:flex;
+  justify-content: space-between;
+}
+</style>
